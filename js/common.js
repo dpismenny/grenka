3a72538976ff12f8746c422a2e6b61a9a7dd790c
dpismenny <denis.pismenny@gmail.com>
2013-07-04 15:39:59 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 
+	// ---------------- CALLBACK POPUP -------------------- //
+		$('.callback').click(function() {
+			if ($(this).hasClass('is-active')) {
+				$(this).removeClass('is-active');
+				$('.callback__popup').slideUp('fast');
+			}
+			else {
+				$(this).addClass('is-active');
+				$('.callback__popup').slideDown('fast');
+			}
+			return false;
+		});
+
+		$('.recent-products__collapse').click(function() {
+			$('.recent-products__carousel').slideToggle('fast');
+		});
+
+		$(document).click(function() {
+			$('.callback').removeClass('is-active');
+			$('.callback__popup').slideUp('fast');
+		});
+
+		$('.callback__popup').click(function(event){
+			event.stopPropagation();
+		});
+
 });
