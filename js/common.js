9843e26ded3839be62dbc69f51c55a3be5057313
dpismenny <denis.pismenny@gmail.com>
2013-07-08 17:36:54 +0300

    update

--- js/common.js
+++ js/common.js
@@ -1,29 +1,58 @@
 $(document).ready(function() {
 
+// init variables
+
+	var popup__callback = $(".js-popup-callback");
+	var popup__city = $(".js-city-popup");
+	var popup__enter = $(".js-popup-enter");
+
 	// ---------------- CALLBACK POPUP -------------------- //
 		$('.callback').click(function() {
-			if ($(this).hasClass('is-active')) {
-				$(this).removeClass('is-active');
-				$('.callback__popup').slideUp('fast');
-			}
-			else {
-				$(this).addClass('is-active');
-				$('.callback__popup').slideDown('fast');
-			}
+			$(this).toggleClass("is-active");
+			popup__callback.toggle();
 			return false;
 		});
 
 		$('.recent-products__collapse').click(function() {
-			$('.recent-products__carousel').slideToggle('fast');
+			$('.recent-products__carousel').slideToggle();
 		});
 
 		$(document).click(function() {
 			$('.callback').removeClass('is-active');
-			$('.callback__popup').slideUp('fast');
+			popup__callback.hide();
+
+			$('.js-change-city').parent().removeClass("is-active");
+			popup__city.hide();
+
+			popup__enter.hide();
 		});
 
-		$('.callback__popup').click(function(event){
+		popup__callback.click(function(event){
 			event.stopPropagation();
 		});
+
+	// ---------------- Change city POPUP -------------------- //
+		$('.js-change-city').click(function() {
+			$(this).parent().toggleClass("is-active");
+			popup__city.toggle();
+			return false;
+		});
+
+		popup__city.click(function(event){
+			event.stopPropagation();
+		});
+
+	// ---------------- Change city POPUP -------------------- //
+		$('.js-enter').click(function() {
+			//$(this).toggleClass("is-active");
+			popup__enter.toggle();
+			return false;
+		});
+
+		popup__enter.click(function(event){
+			event.stopPropagation();
+		});
+
+
 
 });
