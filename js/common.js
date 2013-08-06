fe3d9b96a2fb19ece97358ff1b3a2ad17e39b33f
Yevgen <godun.yevgen@gmail.com>
2013-08-06 14:45:57 +0300

    main cabinet

--- js/common.js
+++ js/common.js
@@ -177,7 +177,10 @@ tabs();
           title : {
               type : 'outside'
           }
-      } 
+      },
+      afterLoad:function(){
+        
+      }
   });
 
   if ($('#slider-left-main').length > 0) {
@@ -369,29 +372,32 @@ $('.js-read-all').click(function() {
 //gallery
 function gallery() {
   var el = $('.js-gallery');
-  el.find('.gallery__slide a').click(function() {
-    if (!$(this).hasClass('is-active')) {
-      var pic = $(this).attr('href');
-      $(this).parent().parent().find('a').removeClass('is-active');
-      $(this).addClass('is-active');
-      $(this).parent().parent().parent().parent().next().find('img').attr('src', pic);      
-    };    
-    return false;
+  el.each(function(){
+    $(this).find('.gallery__slide a').click(function() {
+      if (!$(this).hasClass('is-active')) {
+        var pic = $(this).attr('href');
+        $(this).parent().parent().find('a').removeClass('is-active');
+        $(this).addClass('is-active');
+        $(this).parent().parent().parent().parent().next().find('img').attr('src', pic);      
+      };    
+      return false;
+    });
+    $(this).find('.gallery__up').click(function() {
+      var act = $(this).parent().find('.gallery__slide a.is-active');
+      act.removeClass('is-active');
+      act.parent().prev().find('a').addClass('is-active');
+      var pic = act.parent().prev().find('a').attr('href');
+      $(this).parent().next().find('img').attr('src', pic); 
+    });
+    $(this).find('.gallery__down').click(function() {
+      var act = $(this).parent().find('.gallery__slide a.is-active');
+      act.removeClass('is-active');
+      act.parent().next().find('a').addClass('is-active');
+      var pic = act.parent().prev().find('a').attr('href');
+      $(this).parent().next().find('img').attr('src', pic); 
+    });
   });
-  el.find('.gallery__up').click(function() {
-    var act = $(this).parent().find('.gallery__slide a.is-active');
-    act.removeClass('is-active');
-    act.parent().prev().find('a').addClass('is-active');
-    var pic = act.parent().prev().find('a').attr('href');
-    $(this).parent().next().find('img').attr('src', pic); 
-  });
-  el.find('.gallery__down').click(function() {
-    var act = $(this).parent().find('.gallery__slide a.is-active');
-    act.removeClass('is-active');
-    act.parent().next().find('a').addClass('is-active');
-    var pic = act.parent().prev().find('a').attr('href');
-    $(this).parent().next().find('img').attr('src', pic); 
-  });
+  
 };
 gallery();
 
